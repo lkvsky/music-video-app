ca48aad76ebb54f9398b77ccbf8fe08a7297f23d
Kyle Lucovsky <kyle@lucovsky.com>
2012-09-19 09:39:59 -0700

    updated admin REST api, messing with Backbone, POST function

--- main.py
+++ main.py
@@ -2,56 +2,119 @@ import os
 import webapp2
 import jinja2
 import simplejson
+import urllib2
+import logging
 
-from google.appengine.api import users
 from google.appengine.ext import db
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
-class Video(db.Model):
-    ytid = db.StringProperty()
-    title = db.StringProperty()
-    img = db.StringProperty()
-    embed = db.BooleanProperty()
+class Artist(db.Model):
+    name = db.StringProperty()
+    channel = db.StringProperty()
+    #  [{ytid: id, title: "title", img: thumbnail_url}]
+    videos = db.TextProperty()
+
+    def to_dict(self):
+        artist_dict = {"name": self.name, "channel": self.channel, "videos": self.videos}
+        return artist_dict
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('templates/index.html')
+        template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render())
 
 
-class VideoHandler(webapp2.RequestHandler):
+#Handler for Artist collection
+class ArtistHandler(webapp2.RequestHandler):
     def get(self):
-        videos = []
-        for video in Video.all():
-            videos.append({
-                "id": video.key().id(),
-                "ytid": video.id,
-                "title": video.title,
-                "img": video.img,
-                "embed": video.embed
-            })
-        self.response.out.write(simplejson.dumps(videos))
+        # get all artists from datastore
+        # present artists as json
+        artists = []
+        for artist in Artist.all():
+            artist_obj = artist.to_dict()
+            artists.append(artist_obj)
+        if len(artists) < 1:
+            self.response.status = 400
+            self.response.out.write("There are no artists yet")
+            return
+        self.response.out.write(simplejson.dumps(artists))
 
     def post(self):
+        #  get name and ytid from request
+        #  search youtube for matching videos
+        #  store into new artist
+        #  save and return artist as json
+        name = self.request.get("name", "")
+        channel = self.request.get("channel", "")
+        if len(name) < 1:
+            self.response.status = 400
+            self.response.out.write("You didn't give us a name")
+            return
+        if len(channel) < 1:
+            self.response.status = 400
+            self.response.out.write("You didn't give us a channel")
+            return
+        q = db.Query(Artist)
+        qExist = q.filter("name =", name)
+        if qExist.count() > 0:
+            self.response.status = 400
+            self.response.out.write("This channel already exists")
+            return
+        try:
+            url = "http://gdata.youtube.com/feeds/api/videos?author=%s&v=2&alt=json&format=5" % (channel)
+            response = urllib2.urlopen(url)
+            results = simplejson.loads(response.read())
+            video_list = results.get("feed", {}).get("entry", [])
+            artist_videos = []
+            for video in video_list:
+                artist_videos.append({
+                    "ytid": video["media$group"]["yt$videoid"]["$t"],
+                    "title": video["media$group"]["media$title"]["$t"],
+                    "img": video["media$group"]["media$thumbnail"][0]["url"],
+                    })
+            videos_json = simplejson.dumps(artist_videos)
+            artist = Artist(name=name, channel=channel, videos=videos_json)
+            artist.put()
+            artist_info = artist.to_dict()
+            self.response.out.write(simplejson.dumps(artist_info))
+        except urllib2.HTTPError, e:
+            logging.info(e.read())
+            self.response.status = 400
+            self.response.out.write("Something went wrong")
+
+    def put(self, artist):
+        # get artist from datastore
+        # update entity based on client request
+        # save info
+        q = db.Query(Artist)
+        match = q.filter("name =", artist)
+        if match.count() == 0:
+            return
+        returned_artist = match[0]
         data = simplejson.loads(self.request.body)
-        video = Video(
-            ytid = data["ytid"],
-            title = data["title"],
-            img = data["img"],
-            embed = data["embed"]
-        ).put()
+        returned_artist.name = data["name"]
+        returned_artist.channel = data["channel"]
+        returned_artist.videos = data["videos"]
+        returned_artist.put()
+        return_toclient = returned_artist.to_dict()
+        self.response.out.write(simplejson.dumps(return_toclient))
 
-        self.response.out.write(simplejson.dumps({
-            "id": video.id(),
-            "ytid": data["ytid"],
-            "title": data["title"],
-            "img": data["img"],
-            "embed": data["embed"]
-        }))
+    def delete(self, artist):
+        # get artist (based on artist's name) from datastore
+        # remove artist
+        # return confirmation
+        q = db.Query(Artist)
+        match = q.filter("name =", artist)
+        if match.count() == 0:
+            return
+        match[0].delete()
 
 
-app = webapp2.WSGIApplication([('/', MainPage)],  debug=True)
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/divas', ArtistHandler)
+    ],  debug=True)
---
+++ static/js/Models/station-model.js
@@ -0,0 +1,53 @@
+//inital prototyping
+function populateDOM(vidArray) {
+	$("#diva-results").empty();
+	$("#diva-videos").empty();
+	var templateList = $("#tpl-proto").html();
+	var compiledList = _.template(templateList);
+	var templatePlayer = $("#tpl-proto-player").html();
+	var compiledPlayer = _.template(templatePlayer);
+	$.each(vidArray, function(index, video) {
+		var vidDiv = compiledList({ thumbnail: video.img, title: video.title, ytid: video.id });
+		var player = compiledPlayer({ ytid: video.id });
+		$("#diva-results").append(vidDiv);
+		$("#diva-videos").append(player);
+	});
+}
+
+function parseJSON(json) {
+	var videos = json.data.items;
+	var vidCollection = [];
+	$.each(videos, function(index, video) {
+		var videoObj = {};
+		videoObj.id = video.id;
+		videoObj.title = video.title;
+		videoObj.img = video.thumbnail.sqDefault;
+		if (video.accessControl.embed === "allowed") {
+			videoObj.embed = true;
+		} else {
+			videoObj.embed = false;
+		}
+		vidCollection.push(videoObj);
+	});
+	populateDOM(vidCollection);
+}
+
+function divaSearch(diva) {
+	$.ajax({
+		url: "https://gdata.youtube.com/feeds/api/videos",
+		data: {
+			"q": diva,
+			"key": "AI39si4TJhL042ksIPEJ2e7X4FI0N4ow47lz97Cms1erDN4TmE06CHG3OD1bru-4NWGtGGYm1lEQBAnTTPKY1XME4ehFoXVv-Q",
+			"alt": "jsonc",
+			"v": "2",
+			"max-results": "4"
+		},
+		dataType: "jsonp",
+		success: parseJSON
+	});
+}
+
+$("#diva-search").click(function() {
+	var input = $("#diva-input").val();
+	divaSearch(input);
+});
