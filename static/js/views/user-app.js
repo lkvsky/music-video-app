6499bdcbc0fcae681eefbd1b895da87a2510bc72
lkvsky <kyle@lucovsky.com>
2012-10-20 20:27:21 -0700

    set up random video shuffling

--- static/js/views/user-app.js
+++ static/js/views/user-app.js
@@ -17,7 +17,7 @@ define([
 			},
 
 			initialize: function() {
-				_.bindAll(this, "render");
+				_.bindAll(this, "render", "mixVideos", "mixShuffle");
 				this.divas = new DivaCollection();
 				this.divas.bind("reset", this.render);
 				this.divas.bind("change", this.render);
@@ -32,19 +32,31 @@ define([
 				this.stnTpl = _.template(stationCreator, { divas : this.divas });
 				$(this.el).append(this.stnTpl);
 			},
-
+			// capture diva IDs to create mix and generate shuffle
 			mixMaker: function() {
-				var mixList = [];
 				var values = $("#diva-selector").val();
+				this.mix = new Mix({ values : values });
+				this.mixShuffle();
+			},
+			// create a master list of possible videos for each retreived model
+			mixVideos: function(model) {
+				modelValues = model.attributes.values;
+				var mixList = [];
 				var collection = this.divas;
-				$(values).each(function(value) {
-					var diva = collection.get(values[value]);
+				$(modelValues).each(function(value) {
+					var diva = collection.get(modelValues[value]);
 					var videos = diva.attributes.videos;
 					mixList = mixList.concat(videos);
 				});
-				console.log(mixList);
-				this.mix = new Mix({ values : this.values });
-				this.mix.save();
+				return mixList;
+			},
+			// pull a shuffled mix from the master list to render
+			mixShuffle: function() {
+				this.masterList = this.mixVideos(this.mix);
+				this.shuffle = [];
+				for (i=0; i<10; i++) {
+					this.shuffle.push(this.masterList[Math.floor(Math.random() * this.masterList.length)]);
+				}
 			}
 
 		});
